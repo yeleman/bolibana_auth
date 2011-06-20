498163b9c95bbe7b296b944dbe2a8ff78e37fedd
rgaudin <rgaudin@gmail.com>
2011-06-20 12:46:46 +0000

    added default admin for models

---
+++ admin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+from django.contrib import admin
+
+
+class RoleAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class PermissionAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class AccessAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class ProviderAdmin(admin.ModelAdmin):
+
+    pass
